f206fb0475ca567b2e01015ae5d8665d890fdc59
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-05-08 11:03:50 -0400

    Went with a different approach, solution accepted for May 8th.

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 class Solution {
@@ -7,30 +9,28 @@ class Solution {
     Check if these points make a straight line in the XY plane. */
     public static boolean checkStraightLine(int[][] coordinates) {
 
-        /*  2 dimensions, so we'll postulate that z-value is always 0
-          cx = aybz − azby  -> 0
-          cy = azbx − axbz  -> 0
-          cz = axby − aybx*/
+        Arrays.sort(coordinates, (Comparator<int[]>) (o1, o2) -> {
+            Integer x1 = o1[0];
+            Integer x2 = o2[0];
+            return x1.compareTo(x2);
+        });
 
-        //boolean straight = false;
         int size = coordinates.length;
-        //                              i-1                 i                   i-1                 i
-        int firstProduct = coordinates[0][0] * coordinates[1][1] - coordinates[0][1] * coordinates[1][0];
+
+        if((coordinates[1][0] - coordinates[0][0]) == 0) return false;
+        int slope0 = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
 
         if(size <= 2)   return true;
 
-        for(int i = 2; i < coordinates.length; i++) {
-            //int compare = coordinates[i-1][0] * coordinates[i-(i-2)][1] - coordinates[i-(i-1)][1] * coordinates[i-1][0];
-            int compare = adjust(coordinates, i-1, 0) * adjust(coordinates, i, 1) - adjust(coordinates, i-1, 1) * adjust(coordinates, i, 0);
-            //if(compare != firstProduct)     return false;
-            if(compare == 0) return true;
+        for(int i = 1; i < coordinates.length - 1; i++) {
+            int xDiff = (coordinates[i+1][0] - coordinates[i][0]);
+            if(xDiff == 0) return false;
+            int yDiff = (coordinates[i+1][1] - coordinates[i][1]);
+
+            int slope = (coordinates[i+1][1] - coordinates[i][1]) / xDiff;
+            if(Math.abs(slope) != Math.abs(slope0)) return false;
         }
-        return false;
-    }
-
-    private static int adjust(int[][] coordinates, int i, int j) {
-
-        return coordinates[i][j] - coordinates[i-1][j];
+        return true;
     }
 
     /* May 6th: Majority Element
@@ -60,18 +60,22 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        /* May 6th: Majority Element
-        int[] nums1 = {3,2,3};  // Output: 3
+        // May 6th: Majority Element
+        /*int[] nums1 = {3,2,3};  // Output: 3
         int [] nums2 = {2,2,1,1,1,2,2}; // Output: 2
         System.out.println(majorityElement(nums1));
         System.out.println(majorityElement(nums2)); */
 
-        /* May 8th: Check If It Is a Straight Line */
+        // May 8th: Check If It Is a Straight Line
         int[][] coordinates1 = {{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}};
         int[][] coordinates2 = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
+        int[][] coordinates3 = {{1,2},{1,3},{3,4},{4,5},{5,6},{6,7}};
+        int[][] coordinates4 = {{-4,-3},{1,0},{3,-1},{0,-1},{-5,2}};
 
         System.out.println(checkStraightLine(coordinates1));
         System.out.println(checkStraightLine(coordinates2));
+        System.out.println(checkStraightLine(coordinates3));
+        System.out.println(checkStraightLine(coordinates4));
 
     }
 }
